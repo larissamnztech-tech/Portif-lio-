caf4a12c10d8b8bf87f422b922c96ada3f7ce16c
larissamnztech-tech <larissa.mnztech@gmail.com>
2026-06-20 18:02:14 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
+
+if (menuToggle) {
+  menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("open");
+  });
+}
+
+document.querySelectorAll(".nav-links a").forEach((link) => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("open");
+  });
+});
